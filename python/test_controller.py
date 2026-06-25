ebb2fb72cbb902d773b829bb9317dbede5b1d2fa
LittlePrince-king <yatharth2209@gmail.com>
2026-06-25 10:49:44 -0400

    Update test_controller.py

--- python/test_controller.py
+++ python/test_controller.py
@@ -1,1 +1,19 @@
+class FakeArduino:
+    def write(self, data):
+        print("Arduino received:", data.decode().strip())
 
+
+class ArduinoController:
+    def __init__(self):
+        self.serial = FakeArduino()
+
+    def start(self, angle):
+        self.serial.write(f"{angle}\n".encode())
+
+
+# test
+arduino = ArduinoController()
+
+arduino.start(90)
+arduino.start(45)
+arduino.start(120)
